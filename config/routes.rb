e0543208d5a927f27589495098c385d5c1f0ff8a
scottzyang <scottzyang@gmail.com>
2023-03-20 23:33:02 -0500

    Set up show action controller, view, and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get "/articles", to: "articles#index"
+  root "articles#index"
+
+  resources :articles
 end
